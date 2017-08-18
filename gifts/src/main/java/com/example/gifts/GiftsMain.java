b4819e967692c5a0e90edd298dc40cad6c850371
yuanfei.wang <wyf-shenghuo@163.com>
2017-08-18 17:33:02 +0800

    first

---
+++ gifts/src/main/java/com/example/gifts/GiftsMain.java
@@ -0,0 +1,19 @@
+package com.example.gifts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by wangyuanfei on 2017/8/18.
+ */
+
+public class GiftsMain extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("wyf","q");
+    }
+}
